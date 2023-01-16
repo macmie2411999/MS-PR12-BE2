4663a2ad2a8f1148d9f1029002a39de5fce08546
TênMac <caoducmanh2411999@gmail.com>
2023-01-16 13:49:26 +0300

    Done

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,13 +10,13 @@ def test_healthcheck():
 
 class TestStudents():
     def test_get_empty_staffmember(self):
-        response = requests.get(f'{api_url}/v1/staffmember')
+        response = requests.get(f'{api_url}/v1/staffmembers')
         assert response.status_code == 200
         assert len(response.json()) == 0
 
     def test_create_staffmember(self):
         body = {"NameStaffMember": "Mac Mie 10", "AgeStaffMember": "24", "RoleStaffMember": "Assistant"}
-        response = requests.post(f'{api_url}/v1/staffmember', json=body)
+        response = requests.post(f'{api_url}/v1/staffmembers', json=body)
         assert response.status_code == 200
         assert response.json().get('NameStaffMember') == 'Mac Mie 10'
         assert response.json().get('AgeStaffMember') == '24'
@@ -24,14 +24,14 @@ class TestStudents():
         assert response.json().get('IdStaffMember') == '0'
 
     def test_get_staffmember_by_id(self):
-        response = requests.get(f'{api_url}/v1/staffmember/0')
+        response = requests.get(f'{api_url}/v1/staffmembers/0')
         assert response.status_code == 200
         assert response.json().get('NameStaffMember') == 'Mac Mie 10'
         assert response.json().get('AgeStaffMember') == '24'
         assert response.json().get('RoleStaffMember') == 'Assistant'
         assert response.json().get('IdStaffMember') == '0'
 
-    def test_get_not_empty_stu(self):
-        response = requests.get(f'{api_url}/v1/staffmember')
+    def test_get_not_empty_staffmember(self):
+        response = requests.get(f'{api_url}/v1/staffmembers')
         assert response.status_code == 200
         assert len(response.json()) == 1
